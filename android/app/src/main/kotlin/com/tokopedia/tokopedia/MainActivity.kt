eadd83ebfbb0210ce0abf72b2c585f30a3fa9178
tanya ilham <rhtsgh77@gmail.com>
2023-01-10 12:55:22 +0700

    tokopedia

---
+++ android/app/src/main/kotlin/com/tokopedia/tokopedia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tokopedia.tokopedia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/splashScreen/views/splash_screen_view.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:tokopedia/config/warna.dart';
+
+import '../controllers/splash_screen_controller.dart';
+
+class SplashScreenView extends GetView<SplashScreenController> {
+  @override
+  Widget build(BuildContext context) {
+    double tinggi = MediaQuery.of(context).size.height;
+    double lebar = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Container(
+        height: tinggi,
+        padding: EdgeInsets.fromLTRB(20, 30, 30, 20),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [bgLogin, bgLogin2])),
+        child: Column(
+          children: [
+            Container(
+                width: lebar,
+                height: tinggi * 0.3,
+                // color: Colors.black,
+                margin: EdgeInsets.only(bottom: 20),
+                child: Image.asset("assets/image/sp.png", fit: BoxFit.contain)),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Lets`Get Started",
+                style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              height: 45,
+              child: Text(
+                "Connect with each other with chatting or calling. Enjoy safe and private texting",
+                style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              color: Colors.white,
+              height: 40,
+              width: lebar,
+              child: MaterialButton(
+                color: Colors.white,
+                onPressed: () {},
+                child: Text("Join Now"),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Allready have an account?",
+                    style: TextStyle(fontSize: 15, color: Colors.white),
+                  ),
+                  InkWell(
+                    // onTap: () => Get.toNamed(Routes.),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(fontSize: 15, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
